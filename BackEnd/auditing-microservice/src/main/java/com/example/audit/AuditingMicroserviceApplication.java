72776e44ecee3bbda9ce5e3f8bad09e198e9060e
Deepa Anchan <deepa.anchan97@gmail.com>
2022-04-13 15:40:35 +0530

    Eureka changes

--- BackEnd/auditing-microservice/src/main/java/com/example/audit/AuditingMicroserviceApplication.java
+++ BackEnd/auditing-microservice/src/main/java/com/example/audit/AuditingMicroserviceApplication.java
@@ -2,10 +2,12 @@ package com.example.audit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.jms.annotation.EnableJms;
 
 @EnableJms
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuditingMicroserviceApplication {
 
 	public static void main(String[] args) {
